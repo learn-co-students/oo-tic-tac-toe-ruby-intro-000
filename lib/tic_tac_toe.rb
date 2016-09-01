2e5702563ad9a3a952a91c30be2592fccdac0bac
Yardagan AL Hamed <gooryalhamed@hotmail.com>
2016-09-01 06:46:08 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,4 +1,5 @@
 class TicTacToe
+
   def initialize()
     @board = Array.new(9," ")
   end
@@ -9,47 +10,37 @@ class TicTacToe
     puts "-----------"
     puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
   end
-
-  def input_to_index(user_input)
-    user_input.to_i - 1
-  end
   def position_taken?(location)
-    @board[location] != " " && @board[location] != ""
+   @board[location.to_i] != " " && @board[location.to_i] != ""
   end
-  def valid_move?(index)
-    index.between?(0,8) && !position_taken?(index)
+  def valid_move?(pos)
+    pos.to_i.between?(1,9) && !position_taken?(pos.to_i - 1)
   end
-  def move(index, current_player = "X")
-    @board[index] = current_player
+  def move(location,currentplayer)
+    @board[location.to_i - 1] = currentplayer
   end
-
   def turn
     puts "Please enter 1-9:"
     input = gets.strip
-    index = input_to_index(input)
-    if valid_move?(index)
-      move(index)
+    if valid_move?(input)
+      move(input, current_player)
       display_board
     else
       turn
     end
   end
   def turn_count
-    counter = 0
-    @board.each do |e|
-      if e == "X" || e == "O"
-        counter +=1
-      end
+  @board.count{|el| el == "X" || el == "O"}
+end
+  def current_player
+    if turn_count % 2 == 0
+      return "X"
+    else
+      return "O"
     end
-    return counter
-end
-def current_player
-  if turn_count % 2 == 0
-    return "X"
-  else
-    return "O"
   end
-end
+
+
   WIN_COMBINATIONS = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
   def won?
     WIN_COMBINATIONS.detect {|win_combination|
@@ -71,11 +62,7 @@ end
   end
 
   def over?
-    if full? || won? || draw?
-      return true
-    else
-      return false
-    end
+    won? || draw?
     end
   def winner
     if won?  # there is a winner

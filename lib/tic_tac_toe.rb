aa96044e74b487981403204d2847cacba51b84e2
Emily Croft <emilyhcroft@gmail.com>
2016-10-06 19:54:19 +0000

    Done.

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,144 @@
+class TicTacToe
+  def initialize
+    @board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
+  end
+
+  WIN_COMBINATIONS = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7],
+  [2, 5, 8], [0, 4, 8], [2, 4, 6]]
+
+  def display_board
+      puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
+      puts "-----------"
+      puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
+      puts "-----------"
+      puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+  end
+
+  def move(index, current_player)
+    index = (gets.strip.to_i - 1)
+
+    @board[input] = current_player
+  end
+
+  def position_taken?(index)
+    index = (gets.strip.to_i - 1)
+
+    if @board[index] == " "
+      return false
+    elsif @board[index] == "" || @board[index] == nil
+      return false
+    else
+      return true
+    end
+  end
+
+  def valid_move?(index)
+    index = (gets.strip.to_i - 1)
+
+    if (position_taken?(index) == false) && (index <= 8) && (index >= 0)
+      return true
+    elsif (position_taken?(index) == true) || (index > 8) || (index < 0)
+      return false
+    end
+  end
+
+  def turn
+    puts "Please enter 1-9:"
+
+    index = (gets.strip.to_i - 1)
+
+    if valid_move?(index) == true
+      move(index, current_player)
+    elsif valid_move?(index) == false
+      turn
+    end
+
+    display_board
+  end
+
+  def turn_count
+    counter = 0
+
+    @board.each do |square|
+      if (square == "X") || (square == "O")
+        counter += 1
+      end
+    end
+
+    return counter
+  end
+
+  def current_player
+    character = nil
+    if turn_count % 2 == 0
+      character = "X"
+    elsif turn_count % 2 == 1
+      character = "O"
+    end
+    return character
+  end
+
+  def won?
+    WIN_COMBINATIONS.each do |position|
+      if (@board[position[0]] == "X") && (@board[position[1]] == "X") && (@board[position[2]] == "X")
+        return position
+      elsif (@board[position[0]] == "O") && (@board[position[1]] == "O") && (@board[position[2]] == "O")
+        return position
+      end
+    end
+    return false
+  end
+
+  def full?
+    if @board.all? do |spot|
+      (spot != " ") && (spot != "")
+      end
+      return true
+    else
+      return false
+    end
+  end
+
+  def draw?
+    if !(won?) && full?
+      return true
+    else
+      return false
+    end
+  end
+
+  def over?
+    if won? || draw?
+      return true
+    else
+      return false
+    end
+  end
+
+  def winner
+    winner = nil
+    if won?
+      WIN_COMBINATIONS.each do |position|
+        if (@board[position[0]] == "X") && (@board[position[1]] == "X") && (@board[position[2]] == "X")
+          winner = "X"
+        elsif (@board[position[0]] == "O") && (@board[position[1]] == "O") && (@board[position[2]] == "O")
+          winner = "O"
+        end
+      end
+    end
+
+      return winner
+  end
+
+  def play
+    until over?
+      turn
+    end
+
+    if won?
+      puts "Congratulations #{winner}!"
+    elsif draw?
+      puts "Cats Game!"
+    end
+  end
+end

0edd3d2a5d06472c3111f4d8848ffba1a2d36c99
Tony Ho <ho.tonym@gmail.com>
2018-04-26 14:43:09 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -18,16 +18,16 @@ class TicTacToe
     @board = board
   end
 
-  def won?(board)
+  def won?
 
     WIN_COMBINATIONS.each do |win_index|
         win_index_1 = win_index[0]
         win_index_2 = win_index[1]
         win_index_3 = win_index[2]
 
-        board_position_1 = board[win_index_1]
-        board_position_2 = board[win_index_2]
-        board_position_3 = board[win_index_3]
+        board_position_1 = @board[win_index_1]
+        board_position_2 = @board[win_index_2]
+        board_position_3 = @board[win_index_3]
 
         if board_position_1 =="X" && board_position_2 == "X" && board_position_3 =="X"
           return win_index
@@ -39,15 +39,15 @@ class TicTacToe
   end
 
 
-  def full?(board)
-    board.none? do |position|
+  def full?
+    @board.none? do |position|
       position == nil || position == " "
     end
   end
 
-  def draw?(board)
-  won = won?(board)
-  full = full?(board)
+  def draw?
+  won = won?
+  full = full?
 
     if won == false && full == true
       return true
@@ -61,23 +61,23 @@ class TicTacToe
 
 
 
-  def over?(board)
-    if won?(board) || draw?(board) || full?(board)
+  def over?
+    if won? || draw? || full?
       return true
     end
   end
 
-  def winner(board)
-    winner = won?(board)
+  def winner
+    winner = won?
     if winner != false
-      return "#{board[winner[0]]}"
+      return "#{@board[winner[0]]}"
     end
     return nil
   end
 
-  def turn_count(board)
+  def turn_count
   counter = 0
-    board.each do |numba|
+    @board.each do |numba|
       if numba == "X" || numba == "O"
         counter +=1
       end
@@ -85,8 +85,8 @@ class TicTacToe
   return counter
   end
 
-  def current_player(board)
-    if turn_count(board) % 2 == 0
+  def current_player
+    if turn_count % 2 == 0
       return "X"
     else
       return "O"
@@ -94,42 +94,42 @@ class TicTacToe
   end
 
 
-  def play(board)
+  def play
     max_turns = 0
 
     until max_turns == 9
-        draw = draw?(board)
-        won = won?(board)
-        over = over?(board)
+        draw = draw?
+        won = won?
+        over = over?
 
         if draw
           puts "Cat's Game!"
           break
         elsif won
-          puts "Congratulations #{board[won?(board)[0]]}!"
+          puts "Congratulations #{@board[won?[0]]}!"
           break
         elsif over
           break
         end
 
-        turn(board)
+        turn
         max_turns += 1
     end
 
   end
 
-  def turn(board)
+  def turn
     puts "Please enter 1-9:"
     input = gets.strip
     index = input_to_index(input)
 
-    if valid_move?(board, index)
-      player_token = current_player(board)
-      move(board, index, player_token)
-      display_board(board)
+    if valid_move?(index)
+      player_token = current_player
+      move(index, player_token)
+      display_board
 
     else
-      turn(board)
+      turn
     end
 
   end
@@ -151,12 +151,16 @@ class TicTacToe
     @board[index] = player_token
   end
 
-  def position_taken?(board, location)
-    board[location] != " " && board[location] != ""
+  def position_taken?(index)
+    if @board[index] == "X" || @board[index] == "O"
+      return true
+    else
+      return false
+    end
   end
 
-  def valid_move?(board, index)
-    index.between?(0,8) && !position_taken?(board, index)
+  def valid_move?(index)
+    index.between?(0,8) && position_taken?(index) == false
   end
 
 

13397f4a3fe90e189d8a3db30e2827149894fa6d
Al3nMicL <https://twitter.com/Al3nMicL>
2018-11-10 19:50:28 -0500

    initialize test passed

---
+++ lib/tic_tac_toe.rb
@@ -0,0 +1,11 @@
+# OO Tic Tac Toe game logic
+
+# Define the main Class for the game
+class TicTacToe
+# Init the board
+	def initialize(board = nil)
+		@board = board || Array.new(9, " ")
+	end
+
+
+end

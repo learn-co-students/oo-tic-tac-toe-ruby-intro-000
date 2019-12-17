765af5ee43bb96ec79f196d50c2f8456da6344cd
Benjamin Horns <benhorns@gmail.com>
2019-12-17 17:25:16 +0000

    Done.

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -54,12 +54,12 @@ end
     end
 
     def turn
-           puts "Please enter 1-9:"
+           puts "Welcome to TicTacToe. Please enter 1-9:"
+           display_board
            user_input = gets.strip
            index = input_to_index(user_input)
              if valid_move?(index)
                move(index, current_player)
-               display_board
              else
              turn
            end
@@ -97,7 +97,6 @@ end
      end
 
      def play
-   #binding.pry
     until over?
            turn
    end
